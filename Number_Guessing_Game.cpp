baaeac1cba14d03e1f73595496ba502da9266395
Kaitri-cell <karadiyakaitri@gmail.com>
2025-07-12 12:41:35 +0530

    Update and rename Task1.cpp to Simple_Calculator.cpp

--- Number_Guessing_Game.cpp
+++ Number_Guessing_Game.cpp
@@ -21,17 +21,13 @@ float divide(float num1, float num2){
 int main(){
     float num1, num2;
     char ch;
-    cin>>num1;
-    cin>>ch;
-    cin>>num2;
-
-    cout<<num1<<" ";
-    cout<<ch<<" ";
-    cout<<num2<<endl;
+    cin>>num1>>ch>>num2;
+    
+    cout<<num1<<" "<<ch<<" "<<num2<<endl;
 
     if(ch == '+') cout<<add(num1,num2);
-     else if(ch == '-') cout<<subtract(num1,num2);
-      else if(ch == '*') cout<<multiply(num1,num2);
-        else if(ch == '/') cout<<divide(num1,num2);
-          else cout<<"invalid operator";   
+    else if(ch == '-') cout<<subtract(num1,num2);
+    else if(ch == '*') cout<<multiply(num1,num2);
+    else if(ch == '/') cout<<divide(num1,num2);
+    else cout<<"invalid operator";   
 }
